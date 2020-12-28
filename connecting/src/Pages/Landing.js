1756ddeff7d893e62af820d2c520cca893855884
LeonardoMussatto <23527613+LeonardoMussatto@users.noreply.github.com>
2020-12-28 14:15:49 +0100

    Add Landing text

--- connecting/src/Pages/Landing.js
+++ connecting/src/Pages/Landing.js
@@ -4,9 +4,10 @@ import { useHistory }        from "react-router-dom"
 
 //Components
 import Button          from "../Components/Shared/Button"
-import PlaceholderText from "../Components/Shared/PlaceholderText"
 
 //ENHANCEMENT #17 make the page responsive - support for mobile devices
+//ENHANCEMENT add animations to landing text
+//STYLE the page definitely need some restyling
 
 const Landing = () => {
   const theme = useContext(ThemeContext)
@@ -42,8 +43,10 @@ const Landing = () => {
     <main  style = {page}>
       <h1  style = {title}>Connecting...</h1>
       <div style = {text}>
-        <PlaceholderText marginTB={"5%"} marginLR={"10%"} length={"long"} />
-        <PlaceholderText marginTB={"5%"} marginLR={"10%"} length={"short"} />
+        <p>You have just clicked this link, your friend sent you. Or maybe it was from someone you haven't been hearing much from lately. You should really reach out and see how everything is going. Or perhaps you just remembered you had this link in your bookmarks for some time now and you were wondering what it is. Well...</p>
+        <p>You are about to start a web experience. Take some time. Relax. Sit back on your chair. You didn't even notice but you have been getting closer and closer to the screen. Or maybe you are lying on your bed. Under the cover is better. If you just woke up, you should finish making yourself that cup of coffee before starting. You don't want to forget about it.</p>
+        <p>Now that you have found a comfortable position, you realize you don't have your headphones with you. There is no rush, take them and come back. Or maybe you are one of those who always have their headphones with them. Well, put them on. Let the world around you fade. You don't want to be interrupted. You know what, you should tell the others not to call you for some time: you are about to take a look at that web experience your friend was talking about. Maybe no one heard you... try a second time. Or if you don't want to, let's just hope no one will bother you.</p>
+        <p>Think about anything you absolutely need to do before taking some time off by yourself, and get it done. Otherwise, don't think about it. Now that you are ready, you start searching for a way to start this experience. Here it is. You click it and ...</p>
       </div>
       <Button
         gridPosition = {"side"}
